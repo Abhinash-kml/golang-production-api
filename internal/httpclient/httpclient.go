e3f83c19676e03809499f67d218e72aab1284c23
Abhinash-kml <74886589+Abhinash-kml@users.noreply.github.com>
2026-04-08 13:51:06 +0530

    Finished implementing resilient http client, testing remaining

--- internal/httpclient/httpclient.go
+++ internal/httpclient/httpclient.go
@@ -1,6 +1,9 @@
 package httpclient
 
 import (
+	"context"
+	"fmt"
+	"io"
 	"net/http"
 	"time"
 
@@ -8,14 +11,14 @@ import (
 )
 
 // Client wraps default http client with resilience patterns
-type client struct {
+type Client struct {
 	httpclient *http.Client
 	resilient  *resilience.Resilient
 }
 
 // Constructor function
-func NewClient(config resilience.Config) *client {
-	return &client{
+func NewClient(config resilience.Config) *Client {
+	return &Client{
 		httpclient: &http.Client{
 			Timeout: config.RequestTimeout,
 			Transport: &http.Transport{
@@ -33,4 +36,55 @@ type Response struct {
 	StatusCode int
 	Body       []byte
 	Headers    http.Header
+}
+
+// Get performs a resilient GET request
+func (c *Client) Get(ctx context.Context, url string) (*Response, error) {
+	var response *Response
+
+	err := c.resilient.Execute(ctx, func(ctx context.Context) error {
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+		if err != nil {
+			return err
+		}
+
+		resp, err := c.httpclient.Do(req)
+		if err != nil {
+			// Network errors are retryable
+			return resilience.RetryableError{Err: err}
+		}
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return resilience.RetryableError{Err: err}
+		}
+
+		// 5xx errors are retryable
+		if resp.StatusCode >= 500 {
+			return resilience.RetryableError{
+				Err: fmt.Errorf("server error: %d", resp.StatusCode),
+			}
+		}
+
+		// 4xx errors are not retryable
+		if resp.StatusCode >= 400 {
+			return fmt.Errorf("client error: %d", resp.StatusCode)
+		}
+
+		response = &Response{
+			StatusCode: resp.StatusCode,
+			Body:       body,
+			Headers:    resp.Header,
+		}
+
+		return nil
+	})
+
+	return response, err
+}
+
+// Metrics
+func (c *Client) Metrics() map[string]int64 {
+	return c.resilient.Metrics()
 }
