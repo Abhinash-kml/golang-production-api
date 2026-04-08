7ef37c6f36cb531ae3286bc2db35550b0232a824
Abhinash-kml <74886589+Abhinash-kml@users.noreply.github.com>
2026-04-08 13:35:56 +0530

    Started implementing resilient http client

--- internal/httpclient/httpclient.go
+++ internal/httpclient/httpclient.go
@@ -1,1 +1,36 @@
 package httpclient
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/abhinash-kml/go-api-server/internal/resilience"
+)
+
+// Client wraps default http client with resilience patterns
+type client struct {
+	httpclient *http.Client
+	resilient  *resilience.Resilient
+}
+
+// Constructor function
+func NewClient(config resilience.Config) *client {
+	return &client{
+		httpclient: &http.Client{
+			Timeout: config.RequestTimeout,
+			Transport: &http.Transport{
+				MaxIdleConns:        100,
+				MaxIdleConnsPerHost: 10,
+				IdleConnTimeout:     90 * time.Second,
+			},
+		},
+		resilient: resilience.New(config),
+	}
+}
+
+// Response wraps the HTTP response
+type Response struct {
+	StatusCode int
+	Body       []byte
+	Headers    http.Header
+}
